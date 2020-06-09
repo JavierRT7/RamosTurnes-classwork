f17cf8150e1330eaa72dbb96496b7c31cd308fc6
JavierRT7 <64195545+JavierRT7@users.noreply.github.com>
2020-06-09 12:31:41 +0200

    Update Pong.py

--- pyGame/Pong/Pong.py
+++ pyGame/Pong/Pong.py
@@ -19,12 +19,23 @@ done = False
 ball_width = 20
 x_val = 150
 y_val = 200
-x_direction = 1
-y_direction = 1
+x_direction = -2
+y_direction = -2
 padd_length = 15
 padd_width = 60
 x_padd = 0
 y_padd = 20
+padd_collision = x_padd + 7.5
+padd_collision_y1 = y_padd + 30
+padd_collision_y2 = y_padd - 30
+ai_length = 15
+ai_width = 60
+x_ai = 625
+y_ai = 20
+ai_collision = x_ai + 7.5
+ai_collision_y1 = y_ai + 30
+ai_collision_y2 = y_ai - 30
+ai_direction = 2
 # -- Manages how fast screen refreshes
 clock = pygame.time.Clock()
 ### -- Game Loop
@@ -37,37 +48,74 @@ while not done:
     keys = pygame.key.get_pressed()
         ## - the up key or down key has been pressed
     ### SRC - You can fix this by not including this in the for loop!
-    if keys[pygame.K_UP]:
-          y_padd = y_padd - 5 
-    if keys[pygame.K_DOWN]:
-          y_padd = y_padd + 5
+  if keys[pygame.K_UP]:
+        y_padd = y_padd - 5
+        padd_collision_y1 = y_padd + 30
+        padd_collision_y2 = y_padd - 30
+  if keys[pygame.K_DOWN]:
+        y_padd = y_padd + 5
+        padd_collision_y1 = y_padd + 30
+        padd_collision_y2 = y_padd - 30
   #Next event
   # -- Game logic goes after this comment
-  if x_val == x_padd:  ### SRC - It might not be equal, especially if you make the ball go faster.
-      if y_val == y_padd:
-          x_direction = 1
+  if x_val < 8:  ### SRC - It might not be equal, especially if you make the ball go faster.
+    if y_val < padd_collision_y1:
+      if y_val > padd_collision_y2:
+        x_direction = 3
+        if y_direction == 2:
+          y_direction = 3
+        #End If
+        if y_direction == -2:
+          y_direction = -3
+        #End If  
       #End If
+    #End If
   #End If
-  if x_val == 640:
-      x_direction = -1
+  if x_val > 617:  ### SRC - It might not be equal, especially if you make the ball go faster.
+    if y_val < ai_collision_y1:
+      if y_val > ai_collision_y2:
+        x_direction = -3
       #End If
-  if x_val < -10:
-      x_direction = 1
-      x_val = 150
+    #End If
+  #End If
+  if y_direction > 0:
+      ai_direction = 2
+  #End If
+  if y_direction < 0:
+      ai_direction = -2
+  #End If
+  if y_ai > 460:
+      ai_direction = -2
+  #End If
+  if y_ai < 0:
+      ai_direction = 2
+  #End If
+  if x_val > 640:
+      x_direction = 2
+      x_val = 320
       y_val = 200
       #End If
-  if y_val == 0:
-      y_direction = 1
+  if x_val < -10:
+      x_direction = -2
+      x_val = 320
+      y_val = 200
       #End If
-  if y_val == 480:
-      y_direction = -1
+  if y_val < 0:
+      y_direction = 2
+      #End If
+  if y_val > 480:
+      y_direction = -2
   x_val = x_val + x_direction
   y_val = y_val + y_direction
+  y_ai = y_ai + ai_direction
+  ai_collision_y1 = y_ai + 26
+  ai_collision_y2 = y_ai - 26
   # -- Screen background is BLACK
   screen.fill (BLACK)
   # -- Draw here
   pygame.draw.rect(screen, BLUE, (x_val,y_val,ball_width,ball_width))
   pygame.draw.rect(screen, WHITE, (x_padd, y_padd, padd_length, padd_width))
+  pygame.draw.rect(screen, YELLOW, (x_ai, y_ai, ai_length, ai_width))
   # -- flip display to reveal new position of objects
   pygame.display.flip()
   # - The clock ticks over
