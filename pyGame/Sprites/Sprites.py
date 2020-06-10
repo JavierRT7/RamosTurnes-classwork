648b88e30ef7df44fc23444702298391a99a01df
JavierRT7 <64195545+JavierRT7@users.noreply.github.com>
2020-06-10 14:23:13 +0200

    Update Sprites.py

--- pyGame/Sprites/Sprites.py
+++ pyGame/Sprites/Sprites.py
@@ -28,9 +28,22 @@ class Snow(pygame.sprite.Sprite):
     self.rect.x = random.randrange(0, 600)
     self.rect.y = random.randrange(0, 400)
   #End Procedure
-#End Class
+#End Class
+
 # -- Exit game flag set to false
 done = False
+# Create a list of the snow blocks
+snow_group = pygame.sprite.Group()
+# Create a list of all sprites
+all_sprites_group = pygame.sprite.Group()
+# Create the snowflakes
+number_of_flakes = 50 # we are creating 50 snowflakes
+for x in range (number_of_flakes):
+    my_snow = snow(WHITE, 5, 5) # snowflakes are white with size 5 by 5 px
+    snow_group.add (my_snow) # adds the new snowflake to the group of snowflakes
+    all_sprites_group.add (my_snow) # adds it to the group of all Sprites
+#Next x
+
 # -- Manages how fast screen refreshes
 clock = pygame.time.Clock()
 ### -- Game Loop
@@ -45,6 +58,7 @@ while not done:
   # -- Screen background is BLACK
   screen.fill (BLACK)
   # -- Draw here
+  all_sprites_group.draw (screen)
   # -- flip display to reveal new position of objects
   pygame.display.flip()
   # - The clock ticks over
