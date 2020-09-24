87d56c22ce4ad000687595fc8d7b6ae7fe7d45c4
JavierRT7 <ramosturnesjj@dulwich.org.uk>
2020-09-24 10:18:18 +0100

    update

---
+++ Classwork/add name.py
@@ -0,0 +1,34 @@
+def subroutine(array):
+  choice = 4
+  while choice < 3 or choice > 3:
+    while choice > 3 or choice < 1:
+        print("1. Add name")
+        print("2. Display list")
+        print("3. Quit")
+        choice = input("Enter your choice:")
+        if choice.isdigit:
+            choice = int(choice)
+        else:
+            print("Invalid choice")
+        #End If
+    #End While
+    if choice == 1:
+        name = input("Enter the name to be added to the list")
+        choice = 4
+    #End If
+    if choice == 2:
+        location = input("Enter the position in the list to insert the name:")
+        if location.isdigit:
+            location = int(location)
+            array[location - 1] = name
+            print(location, array[location - 1])
+        else:
+            print("Invalid choice")
+        #End If
+    #End If 
+    choice = 4
+  #End While
+  print("Program terminated")
+#End Procedure
+array = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth"]
+subroutine(array)
