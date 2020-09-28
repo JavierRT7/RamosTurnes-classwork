3de3eb7166ac5379aa69e25ab80358c2be4ea318
JavierRT7 <ramosturnesjj@dulwich.org.uk>
2020-09-28 16:14:40 +0100

    update

---
+++ Classwork/rock paper and, of course, scissors.py
@@ -0,0 +1,28 @@
+playing = True
+player_choice = 1
+computer_choice = 1
+answer = 1
+while playing == True:
+    player_choice = int(input("Enter your choice: enter 1 for rock, 2 for paper or 3 for scissors"))
+    computer_choice = randint(1,4)
+    if player_choice + 2 == computer_choice:
+        print("You win!")
+    elif computer_choice + 2 == player_choice:
+        print("Computer wins!")        
+    elif player_choice + 1 == computer_choice:
+        print("Computer wins!")
+    elif computer_choice + 1 == player_choice:
+        print("You win!")
+    #End If
+    answer = int(input("Would you like to play again? Type 1 for yes or type 2 for no"))
+    if answer == 2:
+        playing = False
+    #End If
+#End While
+
+
+
+
+
+
+
