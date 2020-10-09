51c17195615590f105e6ca87e4dca70122d1add7
JavierRT7 <ramosturnesjj@dulwich.org.uk>
2020-10-09 12:25:49 +0100

    Update word reversal recursion.py

--- Classwork/word reversal recursion.py
+++ Classwork/word reversal recursion.py
@@ -1,8 +1,9 @@
-def reverse(word):
-    if len(word) == 1:
-        return letter
-    else:
-        return rev_word(word[1:]) + letter
-    #End If
-#End Function
-print(reverse('STAR'))
+def rec_func(input_value):
+  item = input_value[0]
+  if len(input_value) == 1:
+    return item
+  else:
+    return rec_func(input_value[1:]) + item
+  #endif
+#end function
+print(rec_func('STAR'))
