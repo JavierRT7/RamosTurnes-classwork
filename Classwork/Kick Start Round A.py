d30ffc0b6752f2942eeb4771e48f449336fe128e
JavierRT7 <ramosturnesjj@dulwich.org.uk>
2020-10-11 18:22:03 +0100

    Update Kick Start Round A.py

--- Classwork/Kick Start Round A.py
+++ Classwork/Kick Start Round A.py
@@ -1,6 +1,14 @@
 cases_T = int(input())
-for counter in range(1, cases_T):
+prices_A = [0, houses_N]
+for counter in range(cases_T):
     houses_N, dollars_B = [int(x) for x in input().split()]
-    
+    for count in range(houses_N):
+        prices_A[count] = int(input())
+    #Next
+    print(prices_A)
+    prices_A.sort()
+    print(prices_A)
+
+
 
  
