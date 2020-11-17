80f117fab85dc28e3a1e9fad6ad6d529e7a0f971
JavierRT7 <ramosturnesjj@dulwich.org.uk>
2020-11-17 11:57:33 +0000

    Update Python Practice.py

--- Classwork/Python Practice.py
+++ Classwork/Python Practice.py
@@ -1,4 +1,8 @@
-aList = [15, 73, 29, 66, 35, 11, 43, 21]
+import random
+aList = []
+for n in range(10):
+    number = random.randint(0, 100)
+    aList.append(number)
 print(aList)
 for j in range(1, len(aList) - 1):
     nextNum = aList[j]
