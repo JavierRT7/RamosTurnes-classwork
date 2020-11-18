a067c2886f6051ecf8ac289c8275034e784cf2e5
JavierRT7 <ramosturnesjj@dulwich.org.uk>
2020-11-18 10:45:11 +0000

    Update mini-project.py

--- pyGame/mini project/mini-project.py
+++ pyGame/mini project/mini-project.py
@@ -1,1 +1,124 @@
+import pygame
+# -- Global Constants
+# -- Colours
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (50,50,255)
+YELLOW = (255,255,0)
+RED = (255,0,0)
+# -- Initialise PyGame
+pygame.init()
+# -- Blank Screen
+size = (1000,720)
+screen = pygame.display.set_mode(size)
+# -- Title of new window/screen
+pygame.display.set_caption("My Window")
+#Classes
+class Player(pygame.sprite.Sprite):
+  # Define the constructor for snow
+  def __init__(self, color, width, height):
+    # Set speed of the sprite
+    self.speed_x = 0
+    self.speed_y = 0
+    # Call the sprite constructor
+    super().__init__()
+    # Create a sprite and fill it with colour
+    self.image = pygame.Surface([width,height])
+    self.image.fill(color)
+    # Set the position of the sprite
+    self.rect = self.image.get_rect()
+    self.rect.x = 20
+    self.rect.y = 20
+  #End Procedure
 
+# Class update function - runs for each pass through the game loop
+  def update(self):
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+      self.speed_y = -2
+      self.rect.y = self.rect.y + self.speed_y
+    #End If
+    if keys[pygame.K_DOWN]:
+      self.speed_y = 2
+      self.rect.y = self.rect.y + self.speed_y
+    #End If
+    if keys[pygame.K_RIGHT]:
+      self.speed_x = 2
+      self.rect.x = self.rect.x + self.speed_x
+    #End If
+    if keys[pygame.K_LEFT]:
+      self.speed_x = -2
+      self.rect.x = self.rect.x + self.speed_x
+    #End If
+#End Class
+class Wall(pygame.sprite.Sprite):
+    # Define the constructor for invader
+    def __init__(self, color, width, height, x_ref, y_ref):
+        # Call the sprite constructor
+        super().__init__()
+        # Create a sprite and fill it with colour
+        self.image = pygame.Surface([width,height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        # Set the position of the player attributes
+        self.rect.x = x_ref
+        self.rect.y = y_ref
+    #End Procedure
+#End Class
+# -- Exit game flag set to false
+map = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
+done = False
+all_sprites_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+wall_group = pygame.sprite.Group()
+player = Player(BLUE, 20, 20)
+player_group.add(player)
+all_sprites_group.add(player)
+for y in range(18):
+    for x in range(18):
+        if map[x][y] == 1:
+            my_wall = Wall(RED, 40, 40, x*40, y *40)
+            wall_group.add(my_wall)
+            all_sprites_group.add(my_wall)
+        #End If
+    #Next
+#Next
+# -- Manages how fast screen refreshes
+clock = pygame.time.Clock()
+### -- Game Loop
+while not done:
+  # -- User input and controls
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      done = True
+    #End If
+  #Next event
+  # -- Game logic goes after this comment
+  all_sprites_group.update()
+  # -- Screen background is BLACK
+  screen.fill (BLACK)
+  # -- Draw here
+  all_sprites_group.draw (screen)
+  # -- flip display to reveal new position of objects
+  pygame.display.flip()
+  # - The clock ticks over
+  clock.tick(60)
+#End While - End of game loop
+pygame.quit()
