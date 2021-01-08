c7fb7d9fc3ec0f077043e1cbfe989bc5ce69458e
JavierRT7 <ramosturnesjj@dulwich.org.uk>
2021-01-08 03:34:43 +0000

    update

---
+++ pyGame/Project/Initial Idea.py
@@ -0,0 +1,82 @@
+import pygame
+# -- Global Constants
+# -- Colours
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (50,50,255)
+YELLOW = (255,255,0)
+BROWN = (155,103,60)
+# -- Initialise PyGame
+pygame.init()
+# -- Blank Screen
+size = (640,480)
+screen = pygame.display.set_mode(size)
+# -- Title of new window/screen
+pygame.display.set_caption("My Window")
+#Classes
+class Brick(pygame.sprite.Sprite):
+    # Define the constructor for invader
+    def __init__(self, x_ref, y_ref):
+        # Call the sprite constructor
+        super().__init__()
+        # Create a sprite and fill it with colour
+        self.image = pygame.image.load('Brick.jpg')
+        self.rect = self.image.get_rect()
+        # Set the position of the player attributes
+        self.rect.x = x_ref
+        self.rect.y = y_ref
+    #End Procedure
+#End Class
+class Door(pygame.sprite.Sprite):
+    # Define the constructor for invader
+    def __init__(self, x_ref, y_ref):
+        # Call the sprite constructor
+        super().__init__()
+        # Create a sprite and fill it with colour
+        self.image = pygame.Surface([40,5])
+        self.image.fill(BROWN)
+        self.rect = self.image.get_rect()
+        # Set the position of the player attributes
+        self.rect.x = x_ref
+        self.rect.y = y_ref
+    #End Procedure
+#End Class
+class Selector(pygame.sprite.Sprite):
+    # Define the constructor for invader
+    def __init__(self, x_ref, y_ref):
+        # Call the sprite constructor
+        super().__init__()
+        # Create a sprite and fill it with colour
+        self.image = pygame.image.load('Selector.png')
+        self.rect = self.image.get_rect()
+        # Set the position of the player attributes
+        self.rect.x = x_ref
+        self.rect.y = y_ref
+    #End Procedure
+#End Class
+# -- Exit game flag set to false
+done = False
+group = pygame.sprite.Group()
+selector = Selector(300, 300)
+group.add(selector)
+# -- Manages how fast screen refreshes
+clock = pygame.time.Clock()
+### -- Game Loop
+while not done:
+  # -- User input and controls
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      done = True
+    #End If
+  #Next event
+  # -- Game logic goes after this comment
+  # -- Screen background is BLACK
+  screen.fill (YELLOW)
+  # -- Draw here
+  group.draw(screen)
+  # -- flip display to reveal new position of objects
+  pygame.display.flip()
+  # - The clock ticks over
+  clock.tick(60)
+#End While - End of game loop
+pygame.quit()
