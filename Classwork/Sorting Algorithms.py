8c488e0f5a7d6d500ec53097bf10e7bf165f54fc
JavierRT7 <ramosturnesjj@dulwich.org.uk>
2021-04-28 10:44:32 +0100

    Update Sorting Algorithms.py

--- Classwork/Sorting Algorithms.py
+++ Classwork/Sorting Algorithms.py
@@ -9,6 +9,10 @@ def bubbleSort(numbers):
                 temp = numbers[counter]
                 numbers[counter] = numbers[counter + 1]
                 numbers[counter + 1] = temp
+            #End If
+        #Next
+    #Next
+#End Procedure
 
 def insertionSort(numbers):
     for count in range(len(numbers)):
@@ -17,32 +21,45 @@ def insertionSort(numbers):
         while counter >= 0 and numbers[counter] > nextNum:
             numbers[counter + 1] = numbers[counter]
             counter = counter - 1
+        #End While
         numbers[counter + 1] = nextNum
+    #Next
+#End Procedure
+
+def inbuiltSort(numbers):
+    numbers.sort()
+#End Procedure
 
 numbers = []
-for counter in range(10):
+for counter in range(1000):
     numbers.append(random.randint(1, 1000))
 #Next
+
 startbubble = time.time()
 bubbleSort(numbers)
 endbubble = time.time()
 print("done")
+
 numbers = []
-for counter in range(1000000):
+for counter in range(1000):
     numbers.append(random.randint(1, 1000))
 #Next
+
 startpython = time.time()
-numbers.sort()
+inbuiltSort(numbers)
 endpython = time.time()
 print("done")
+
 numbers = []
-for counter in range(10000):
+for counter in range(1000):
     numbers.append(random.randint(1, 1000))
 #Next
+
 startinsert = time.time()
 insertionSort(numbers)
 endinsert = time.time()
 print("done")
+
 print("The bubble sort took", endbubble - startbubble, "seconds")
 print("The in-built sort function took", endpython - startpython, "seconds")
 print("The insertion sort took", endinsert - startinsert, "seconds")
