845973f97328d43d12d14cb760059db3492e32a8
JavierRT7 <ramosturnesjj@dulwich.org.uk>
2021-05-19 09:17:31 +0100

    Update fibonacci.py

--- Classwork/fibonacci.py
+++ Classwork/fibonacci.py
@@ -1,3 +1,4 @@
+fibonacci = []
 old_value = 1
 value = 1
 print(old_value)
@@ -5,5 +6,6 @@ print(value)
 for counter in range(100):
     temp = value + old_value
     print(temp)
+    fibonacci.append(temp)
     old_value = value
     value = temp
