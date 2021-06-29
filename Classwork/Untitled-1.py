3442b7c81315470e96a79dda7ce2c1aba462bf37
JavierRT7 <ramosturnesjj@dulwich.org.uk>
2021-06-29 10:23:42 +0100

    Update Untitled-1.py

--- Classwork/Untitled-1.py
+++ Classwork/Untitled-1.py
@@ -1,46 +1,51 @@
-answer = input("Are you American?")
-if answer == "yes":
-    print("Fuck You!")
-else:
-    answer = input("Is it savoury?")
+again = True
+while again == True:
+    answer = input("Are you American?")
     if answer == "yes":
-        answer = input("Is it steamed?")
-        if answer == "yes":
-            answer = input("Does it contain suet?")
-            if answer == "yes":
-                answer = input("Does it contain flour?")
-                if answer == "yes":
-                    print("It is a pudding!")
-                else:
-                    print("It is not a pudding")
-            else:
-                print("It's not a pudding")
-        else:
-            print("It is not a pudding")
+        print("Fuck You!")
     else:
-        answer = input("Does it contain milk?")
+        answer = input("Is it savoury?")
         if answer == "yes":
-            answer = input("Does it contain fruit?")
+            answer = input("Is it steamed?")
             if answer == "yes":
-                answer = input("Does it contain egg?")
+                answer = input("Does it contain suet?")
                 if answer == "yes":
-                    answer = input("Is it steamed or baked?")
+                    answer = input("Does it contain flour?")
                     if answer == "yes":
                         print("It is a pudding!")
-                        answer = input("Is it milk-thickened?")
-                        if answer == "yes":
-                            print("It is a blancmange!!! not a pudding idiot!")
-                        else:
-                            print("It's still just a pudding")
                     else:
-                        print("It's not a pudding")
+                        print("It is not a pudding")
                 else:
                     print("It's not a pudding")
             else:
                 print("It is not a pudding")
         else:
-            answer = input("Is it gelatine based?")
+            answer = input("Does it contain milk?")
             if answer == "yes":
-                print("It is a jelly! not a pudding!")
+                answer = input("Does it contain fruit?")
+                if answer == "yes":
+                    answer = input("Does it contain egg?")
+                    if answer == "yes":
+                        answer = input("Is it steamed or baked?")
+                        if answer == "yes":
+                            print("It is a pudding!")
+                            answer = input("Is it milk-thickened?")
+                            if answer == "yes":
+                                print("It is a blancmange!!! not a pudding idiot!")
+                            else:
+                                print("It's still just a pudding")
+                        else:
+                            print("It's not a pudding")
+                    else:
+                        print("It's not a pudding")
+                else:
+                    print("It is not a pudding")
             else:
-                print("It is nothing")
+                answer = input("Is it gelatine based?")
+                if answer == "yes":
+                    print("It is a jelly! not a pudding!")
+                else:
+                    print("It is nothing")
+    answer = input("Do you want to go again?")
+    if answer == "no":
+        again = False
